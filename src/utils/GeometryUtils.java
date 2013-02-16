1c0d8da12b163436e6a79aa287278d7453f3266e
HitTigerTonight <hengh2009@gmail.com>
2013-02-16 14:27:21 +0800

    add geometry utils
    add geometry utils

---
+++ src/utils/GeometryUtils.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)GeometryUtils.java, 2013-2-16. 
+ * 
+ * Copyright 2013 Yodao, Inc. All rights reserved.
+ * YODAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package utils;
+
+/**
+ *
+ * @author Administrator
+ *
+ */
+public class GeometryUtils {
+
+}
