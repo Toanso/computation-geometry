c0cce455b1aad2a2e33786cc4653a40240df596a
HitTigerTonight <hengh2009@gmail.com>
2013-02-17 17:56:54 +0800

    add some questino to be solved
    smallest circle overing problem

---
+++ src/data/Polygon.java
@@ -0,0 +1,38 @@
+
+package data;
+
+/**
+ *
+ * @author liwt
+ *
+ */
+public class Polygon {
+
+    public Point2d points[];
+    public Polygon(){}
+    public Polygon(Point2d points[]){
+        int length = points.length;
+        this.points = new Point2d[length];
+        for(int i=0; i<points.length; i++){
+            this.points[i] = new Point2d(points[i]);
+        }
+    }
+    public Polygon(Polygon p){
+        int length = p.points.length;
+        this.points = new Point2d[length];
+        for(int i=0; i<length; i++){
+            this.points[i] = new Point2d(p.points[i]);
+        }
+    }
+    /**
+     * calculate area of the polygon 
+     * @return
+     */
+    public double area(){
+        double result = 0.0;
+        for(int i=1; i<this.points.length-1; i++){
+            result+= this.points[i].sub(this.points[0]).crossProduct(this.points[i+1].sub(this.points[0]));
+        }
+        return Math.abs(result)/2;
+    }
+}
--- src/data/Triangle.java
+++ src/data/Triangle.java
@@ -20,4 +20,20 @@ public class Triangle {
         B = new Point2d(t.B);
         C = new Point2d(t.C);
     }
+    /**
+     * calculate area of the triangle
+     * @return
+     */
+    public double area(){
+        Vector2d AC = new Vector2d(A, C);
+        Vector2d AB = new Vector2d(A, B);
+        Vector2d BC = new Vector2d(B, C);
+        
+        double len1 = AC.length();
+        double len2 = AB.length();
+        double len3 = BC.length();
+        double p = 1/2*(len1+len2+len3);
+        return Math.sqrt(p*(p-len1)*(p-len2)*(p-len3));
+    }
+    
 }
